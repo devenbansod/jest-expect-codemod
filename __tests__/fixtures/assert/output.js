44355f1fd245fe1322e90598a1ba627404b397fc
Cole Turner <turner.cole@gmail.com>
2019-05-23 11:06:06 -0700

    Update output.js

--- __tests__/fixtures/assert/output.js
+++ __tests__/fixtures/assert/output.js
@@ -1,13 +1,13 @@
 module.exports = function() {
   const a = 1;
-  expect(a).toBe(1);;
+  expect(a).toBe(1);
 
   const b = {
     a: 2,
   };
   expect(b).toEqual({
     a: 2,
-  });;
+  });
 
   const c = true;
   expect(c).toBeTruthy();
